b0aa2d9eaa25b8152426a521ed684a4dc8ea9e6d
James Bruska <bruskatech@protonmail.com>
2026-07-10 17:10:46 -0400

    Update the constants for Turing server

--- cmlreaders/constants.py
+++ cmlreaders/constants.py
@@ -11,171 +11,171 @@ PYFR_SUBJECT_CODE_PREFIXES = ("BW", "CH", "CP", "FR", "FZ", "TJ", "UP")
 
 rhino_paths = {
     # data indices
-    "r1_index": ["protocols/r1.json"],
-    "ltp_index": ["protocols/ltp.json"],
-    "pyfr_index": ["data/events/pyFR/pyFR.json"],
+    "r1_index": ["/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/r1.json"],
+    "ltp_index": ["/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/ltp.json"],
+    "pyfr_index": ["groups/nxh177-lab-herzlab/raw_data/kahana/pyFR/pyFR.json"],
     # root directory to look for pyFR data
-    "pyfr_root": ["data/events/pyFR"],
+    "pyfr_root": ["groups/nxh177-lab-herzlab/raw_data/kahana/pyFR"],
     # Localization-level (subject + localization_
     "localization": [
-        "protocols/{protocol}/subjects/{subject}/localizations/{localization}/neuroradiology/current_processed/localization.json",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/localizations/{localization}/neuroradiology/current_processed/localization.json",
     ],
     # Montage-level (subject + montage)
     "voxel_coordinates": [
-        "data10/RAM/subjects/{subject_montage}/tal/VOX_coords_mother.txt",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/VOX_coords_mother.txt",
     ],
     "prior_stim_results": [
-        "data10/eeg/freesurfer/subjects/{subject_montage}/prior_stim/{subject}_allcords.csv",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/data/eeg/freesurfer/subjects/{subject_montage}/prior_stim/{subject}_allcords.csv",
     ],
     "electrode_coordinates": [
-        "data10/RAM/subjects/{subject_montage}/tal/coords/electrode_coordinates.csv",
-        "data10/RAM/subjects/{subject_montage}/tal/electrode_coordinates.csv",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/coords/electrode_coordinates.csv",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/electrode_coordinates.csv",
     ],
     "mni_coordinates": [
-        "data10/RAM/subjects/{subject_montage}/imaging/autoloc/electrodenames_coordinates_mni.csv"
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/imaging/autoloc/electrodenames_coordinates_mni.csv"
     ],
     "jacksheet": [
-        "data10/RAM/subjects/{subject_montage}/docs/jacksheet.txt",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/docs/jacksheet.txt",
     ],
     "area": [
-        "data10/RAM/subjects/{subject_montage}/docs/area.txt",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/docs/area.txt",
     ],
     "electrode_categories": [
-        "data10/RAM/electrode_categories/{subject_montage}_electrode_categories.txt"  # ,
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/electrode_categories/{subject_montage}_electrode_categories.txt"  # ,
         # 	'data10/RAM/subjects/{subject_montage}/docs/electrode_categories.txt', # 2020-01-07 JS-We decided to only use a single, master folder
         #         'scratch/pwanda/electrode_categories/{subject_montage}_electrode_categories.txt',
         #         'scratch/pwanda/electrode_categories/electrode_categories_{subject_montage}.txt',
     ],
     "good_leads": [
-        "data10/RAM/subjects/{subject_montage}/tal/good_leads.txt",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/good_leads.txt",
     ],
     "leads": [
-        "data10/RAM/subjects/{subject_montage}/tal/leads.txt",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/leads.txt",
     ],
     "classifier_excluded_leads": [
-        "data10/RAM/subjects/{subject_montage}/tal/classifier_excluded_leads.txt",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/classifier_excluded_leads.txt",
     ],
     "matlab_bipolar_talstruct": [
-        "data10/RAM/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database_bipol.mat"
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database_bipol.mat"
     ],
     "matlab_monopolar_talstruct": [
-        "data10/RAM/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database_monopol.mat"
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database_monopol.mat"
     ],
     "pairs": [
-        "protocols/{protocol}/subjects/{subject}/localizations/{localization}/montages/{montage}/neuroradiology/current_processed/pairs.json",
-        "data/eeg/{subject_montage}/tal/{subject_montage}_talLocs_database_bipol.mat",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/localizations/{localization}/montages/{montage}/neuroradiology/current_processed/pairs.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database_bipol.mat",
     ],
     "matlab_pairs": [
-        "data/eeg/{subject_montage}/tal/{subject_montage}_talLocs_database_bipol.mat",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database_bipol.mat",
     ],
     "contacts": [
-        "protocols/{protocol}/subjects/{subject}/localizations/{localization}/montages/{montage}/neuroradiology/current_processed/contacts.json",
-        "data/eeg/{subject_montage}/tal/{subject_montage}_talLocs_database_monopol.mat",
-        "data/eeg/{subject_montage}/tal/{subject_montage}_talLocs_database.mat",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/localizations/{localization}/montages/{montage}/neuroradiology/current_processed/contacts.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database_monopol.mat",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database.mat",
     ],
     "matlab_contacts": [
-        "data/eeg/{subject_montage}/tal/{subject_montage}_talLocs_database_monopol.mat",
-        "data/eeg/{subject_montage}/tal/{subject_montage}_talLocs_database.mat",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database_monopol.mat",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject_montage}/tal/{subject_montage}_talLocs_database.mat",
     ],
     # Report Data
     "session_summary": [
-        "data10/RAM/report_database/{subject_montage}_{experiment}_{session}_session_summary.h5",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/report_database/{subject_montage}_{experiment}_{session}_session_summary.h5",
     ],
     "classifier_summary": [
-        "data10/RAM/report_database/{subject_montage}_{experiment}_{session}_classifier_session_{session}.h5",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/report_database/{subject_montage}_{experiment}_{session}_classifier_session_{session}.h5",
     ],
     "math_summary": [
-        "data10/RAM/report_database/{subject_montage}_{experiment}_{session}_math_summary.h5",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/report_database/{subject_montage}_{experiment}_{session}_math_summary.h5",
     ],
     "target_selection_table": [
-        "data10/RAM/report_database/{subject_montage}_{experiment}_*_target_selection_table.csv",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/report_database/{subject_montage}_{experiment}_*_target_selection_table.csv",
     ],
     "baseline_classifier": [
-        "data10/RAM/report_database/{subject_montage}_retrained_classifier.zip",
-        "data10/RAM/report_database/{subject_montage}_{experiment}_all_retrained_classifier.zip",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/report_database/{subject_montage}_retrained_classifier.zip",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/report_database/{subject_montage}_{experiment}_all_retrained_classifier.zip",
     ],
     # Session Data
     "all_events": [
-        "protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/behavioral/current_processed/all_events.json",
-        "data/events/pyFR/{subject_montage}_events.mat",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/behavioral/current_processed/all_events.json",
+        "groups/nxh177-lab-herzlab/raw_data/kahana/pyFR/{subject_montage}_events.mat",
     ],
     "task_events": [
-        "protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/behavioral/current_processed/task_events.json",
-        "data/events/pyFR/{subject_montage}_events.mat",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/behavioral/current_processed/task_events.json",
+        "groups/nxh177-lab-herzlab/raw_data/kahana/pyFR/{subject_montage}_events.mat",
     ],
     "math_events": [
-        "protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/behavioral/current_processed/math_events.json",
-        "data/events/pyFR/{subject_montage}_math.mat",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/behavioral/current_processed/math_events.json",
+        "groups/nxh177-lab-herzlab/raw_data/kahana/pyFR/{subject_montage}_math.mat",
     ],
     "ps4_events": [
-        "protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/behavioral/current_processed/ps4_events.json"
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/behavioral/current_processed/ps4_events.json"
     ],
     "sources": [
-        "protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/ephys/current_processed/sources.json",
-        "data/eeg/{subject}/eeg.noreref/{eeg_basename}.params.txt",
-        "data/eeg/{subject}/eeg.noreref/params.txt",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/ephys/current_processed/sources.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/data/eeg/{subject}/eeg.noreref/{eeg_basename}.params.txt",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/data/eeg/{subject}/eeg.noreref/params.txt",
     ],
     # Processed EEG data basename
     # For data in /protocols, this gets expanded into either a bunch of files or
     # a single HDF5 file in the case of later RAM subjects recorded on the ENS.
     "processed_eeg": [
-        "protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/ephys/current_processed/noreref/{basename}"
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/{protocol}/subjects/{subject}/experiments/{experiment}/sessions/{session}/ephys/current_processed/noreref/{basename}"
     ],
     # Ramulator-related information
     "experiment_log": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/experiment.log",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/experiment.log",
     ],
     "session_log": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/session.log",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/session.log",
     ],
     "session_json": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/session.json",
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/session.jsonl",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/session.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/session.jsonl",
     ],
     "ramulator_session_folder": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/*",
-        "protocols/r1/subjects/{subject}/experiments/{experiment}/sessions/{session}/ephys/current_source/host_pc/*",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/*",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/r1/subjects/{subject}/experiments/{experiment}/sessions/{session}/ephys/current_source/host_pc/*",
     ],
     # Elemem-related information
     "elemem_session_folder": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/*",
-        "protocols/r1/subjects/{subject}/experiments/{experiment}/sessions/{session}/ephys/current_source/elemem/*",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/*",
+        "/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/r1/subjects/{subject}/experiments/{experiment}/sessions/{session}/ephys/current_source/elemem/*",
     ],
     # There can be multiple timestamped folders for the host pc files for when
     # a session is restarted
     "event_log": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/event_log.json",
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/{timestamped_dir}/event.log",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/event_log.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/{timestamped_dir}/event.log",
     ],
     "experiment_config": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/experiment_config.json",
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/{timestamped_dir}/experiment_config.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/experiment_config.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/{timestamped_dir}/experiment_config.json",
     ],
     "raw_eeg": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/eeg_timeseries.h5",
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/{timestamped_dir}/eeg_data.edf",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/eeg_timeseries.h5",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/{timestamped_dir}/eeg_data.edf",
     ],
     "archived_eeg": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/{timestamped_dir}/eeg_archive/eeg_data.edf",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/elemem/{timestamped_dir}/eeg_archive/eeg_data.edf",
     ],
     "odin_config": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/{subject}_*.csv",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/{subject}_*.csv",
     ],
     # There can be multiple classifiers if artifcat detection was enabled and
     # the classifier needed to be retrained. The order is important here In
     # general, the files should be listed in order of preference so that the
     # first file found is returned
     "used_classifier": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/retrained_classifier/{subject}-classifier.zip",
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/{subject}-classifier.zip",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/retrained_classifier/{subject}-classifier.zip",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/{subject}-classifier.zip",
     ],
     "excluded_pairs": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/retrained_classifier/excluded_pairs.json",
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/excluded_pairs.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/retrained_classifier/excluded_pairs.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/excluded_pairs.json",
     ],
     "all_pairs": [
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/pairs.json",
-        "data10/RAM/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/retrained_classifier/pairs.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/pairs.json",
+        "/groups/nxh177-lab-herzlab/raw_data/kahana/subjects/{subject}/behavioral/{experiment}/session_{session}/host_pc/{timestamped_dir}/config_files/retrained_classifier/pairs.json",
     ],
     # scalp experiments subject info
     "subject_info": "/data/eeg/scalp/ltp/{experiment}/cmldb_subj_info_{experiment}.txt",
@@ -254,7 +254,7 @@ elemem_files = (
 )
 
 # Offset corrections for retrieval events
-offset_corrections = ["protocols/r1/offset_corrections.csv"]
+offset_corrections = ["/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/r1/offset_corrections.csv"]
 
 # Countdown event list corrections
-countdown_errors = ["protocols/r1/countdown_errors.csv"]
+countdown_errors = ["/groups/nxh177-lab-herzlab/processed_data/kahana/protocols/r1/countdown_errors.csv"]
